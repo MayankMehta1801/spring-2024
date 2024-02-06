8676e8c8a5a874c1674b980851b6269cfb53771c
Prashant Srivastav <147681761+its-prashant-srivastav@users.noreply.github.com>
2024-02-06 10:07:25 +0530

    Update 2_factorial.c

--- 30-01-2024/2_factorial.c
+++ 30-01-2024/2_factorial.c
@@ -5,13 +5,28 @@ int main(void){
     // initialising num for input and fact is going to be our output
     int num, fact=1;
     scanf("%d", &num);
-    // loop iterates through an integer i and multiplies the incrementing values of i 
-    // to fact
-    for(int i=1; i<=num; i++){
-        fact *= i;
+    
+    // if num is negative it returns -1
+    if (num<0) { 
+        fact = -1;
     }
+    
+    // 0! = 1
+    if (num == 0){
+        fact = 1;
+    }
+    
+    // if number is positive
+    if (num>0){
+      // loop iterates through an integer i and multiplies the incrementing values of i 
+        // to fact
+        for(int i=1; i<=num; i++){
+            fact *= i;
+    }  
+    }
+    
     // we print it nicely in the end
-    printf("%d! = FACTORIAL(%d) = %d",num,num,  fact);
+    printf("%d", fact);
 
     return 0;
 }
