808150d6d9fb69527d1b92960f2e72a8bd42844f
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-02-11 22:34:30 +0530

    Create 4_transpose_of_matrix.c

---
+++ 06-02-2024/4_transpose_of_matrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void){
+    // taking the input of rows and columns 
+    // constraint: 1 <= n,m <= 10
+    int n,m;
+    scanf("%d %d", &n, &m);
+
+    // we initialise the transpose matrix
+    int transpose[m][n];
+
+    // taking input of the matrix
+    // we assign the reverse value in this loop itself
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%d", &transpose[j][i]);
+        }
+        printf("\n");
+    }
+
+    // now we print the transpose
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            printf("%d " , transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
+}
